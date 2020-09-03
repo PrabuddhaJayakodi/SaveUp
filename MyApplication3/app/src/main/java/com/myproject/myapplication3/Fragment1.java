a1c864817510cd889f848ec3294658b819a76006
Niwantha Ranawaka <44175668+NiwanthaR@users.noreply.github.com>
2020-09-03 12:28:09 +0530

    Map Update

--- MyApplication3/app/src/main/java/com/myproject/myapplication3/Fragment1.java
+++ MyApplication3/app/src/main/java/com/myproject/myapplication3/Fragment1.java
@@ -1,19 +1,43 @@
 package com.myproject.myapplication3;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 public class Fragment1 extends Fragment {
+
+    public View v;
+    private Button button;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment1_layout,container,false);
+        v = inflater.inflate(R.layout.fragment1_layout,container,false);
+
+        //ui declaretion
+        UI_Declare();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(),MapDemo.class));
+            }
+        });
+
+
+        return v;
+    }
+
+    private void UI_Declare() {
+        button = v.findViewById(R.id.button2);
     }
 }
