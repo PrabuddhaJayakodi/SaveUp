9fcda29e5307907d702a5e31c8675b843b05fee7
Niwantha Ranawaka <44175668+NiwanthaR@users.noreply.github.com>
2020-10-13 14:06:48 +0530

    profile UI complete and Contact us complete

--- MyApplication3/app/src/main/java/com/myproject/myapplication3/contact_us.java
+++ MyApplication3/app/src/main/java/com/myproject/myapplication3/contact_us.java
@@ -2,13 +2,33 @@ package com.myproject.myapplication3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class contact_us extends AppCompatActivity {
+
+    private Button get_call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+
+        get_call = findViewById(R.id.gc_btn_call);
+
+        get_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String number = "0711079823";
+
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+number));
+                startActivity(intent);
+            }
+        });
     }
 }
